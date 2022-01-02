6beee8ca72cfe4f20310535d9789649be7728e0a
WillStonebridge <82773632+WillStonebridge@users.noreply.github.com>
2022-01-02 16:12:05 -0500

    Add files via upload

--- Slides.js
+++ Slides.js
@@ -1,42 +1,55 @@
-var index = 0; //the number corresponding to the current slide
-var slides = document.getElementsByClassName('slide'); //all slides
-var numSlides = slides.length; //the number of total slides
 
-//finds the slide with the greatest height and assigns it to a variable
-var height = slides[1].style.height;
-for (let i = 1; i < numSlides; i++) {
-    if (slides[i].style.height > height) {
-        height = slides[i].style.height;
-    }
-}
+var skillSlides = document.getElementsByClassName('skillSlide'); //the skill slides
+var skillIndex = document.getElementById('skillIndex'); //the number beneath the skill slides
+var skillHeader = document.getElementsByClassName('sectionHead')[1]; //the Header for the skill section
 
-//reveals the first slide at startup
-showSlide(index);
+var reviewSlides = document.getElementsByClassName('reviewSlide'); //the review slides
+var reviewIndex = document.getElementById('reviewIndex'); //the number beneath the review slides
 
-function changeSlide(n) {
+//reveals the initial slides of each section at startup
+showSlide(0, skillIndex, skillSlides);
+showSlide(0, reviewIndex, reviewSlides);
+
+//changes to the slideshow index by +/- 1 slides (n), handles looping
+function changeSlide(n, currentIndex, section) {
+
+    var index = currentIndex.innerText[0] - 1; //acquires the index of the current slide in numerical form
 
     if (n + index < 0) { //reverts to the last slide if -1 is reached
-        showSlide(numSlides - 1);
-    } else if (n + index >= numSlides) { //returns to the first stage if a index above the available slides is reached
-        showSlide(0);
+        showSlide(section.length - 1, currentIndex, section);
+    } else if (n + index >= section.length) { //returns to the first slide if the last index is passed
+        showSlide(0, currentIndex, section);
     } else { //otherwise simply adds or subtracts the n to change slides
-        showSlide(index + n);
+        showSlide(index + n, currentIndex, section);
     }
 
 }
 
-function showSlide(n) {
+//displays n slide to for a given section
+function showSlide(n, currentIndex, section) {
 
     //hides the current slide
-    slides[index].style.display = "none";
+    section[currentIndex.innerText[0] - 1].style.display = "none";
 
     //displays slide n
-    slides[n].style.display = "block";
-    slides[n].style.height = height;
-
-    //updates the slide index
-    index = n;
+    section[n].style.display = "block";
 
     //updates the index next to the arrows
-    document.getElementById('slideIndex').innerText = (index + 1) + ' / ' + numSlides;
+    currentIndex.innerText = (n + 1) + ' / ' + section.length;
+
+    //if the slide being changed is in skills, update the section header
+    if(section[0].className === 'skillSlide') {
+        updateSkillHeader(n);
+    }
+}
+
+function updateSkillHeader(n) {
+
+    if(n === 0) {
+        skillHeader.innerText = '- Programming Languages -';
+    } else if (n === 1) {
+        skillHeader.innerText = '- Skills -';
+    } else if (n === 2) {
+        skillHeader.innerText = '- Awards and Achievements -';
+    }
 }
